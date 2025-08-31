843628da0e92d9372c3e3a68ec3778d5a12a0054
Kye Gomez <kye@swarms.world>
2025-08-30 20:44:58 -0700

    examples

--- examples/web_scraping/news_scraper_example.py
+++ examples/web_scraping/news_scraper_example.py
@@ -8,7 +8,7 @@ using the detailed format to get comprehensive information.
 from swarms_tools.search.web_scraper import scrape_and_format_sync
 
 # Scrape a news article with detailed information
-url = "https://techcrunch.com"
+url = "https://x.com"
 content = scrape_and_format_sync(
     url,
 )
