a76838c2d460f229c13555ed3e4c73dd144facf7
Aksh Parekh <parekh.aksh.c@gmail.com>
2025-09-05 22:57:43 -0700

    self-improving agent code changing tool

--- swarms_tools/search/__init__.py
+++ swarms_tools/search/__init__.py
@@ -4,10 +4,12 @@ from swarms_tools.search.web_scraper import (
     scrape_multiple_urls_sync,
 )
 from swarms_tools.search.firecrawl import crawl_entire_site_firecrawl
+from swarms_tools.search.self_evolve import modify_file_content
 
 __all__ = [
     "exa_search",
     "scrape_and_format_sync",
     "scrape_multiple_urls_sync",
     "crawl_entire_site_firecrawl",
+    "modify_file_content",
 ]
